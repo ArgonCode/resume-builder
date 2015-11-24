0810cd3f22bbbd5feb663ee93a14a758df286840
Sylwia M. Bialczak <argoncode18@gmail.com>
2015-11-24 13:08:14 -0800

    Add javascript to display bio data.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,27 +1,27 @@
 var bio = {
-    "name": "",
-    "role": "",
+    "name": "Sylwia Bialczak",
+    "role": "Web Developer",
     "contacts": {
-        "mobile": "",
-        "twitter": "",
-        "github": "",
-        "location": "",
+        "mobile": "555-555-5555",
+        "twitter": "@argoncode",
+        "github": "argoncode",
+        "location": "Portland, OR",
         "facebook": "",
         "googlePlus": "",
         "linkedin": "",
-        "email": "",
+        "email": "argoncode18@gmail.com",
         "kaybase": ""
     },
-    "biopic": "",
-    "welcomeMessage": "",
-    "skills": []
+    "biopic": "images/sylwia.jpeg",
+    "welcomeMessage": "This is welcome message that gets displayed",
+    "skills": ["one", "two"]
 };
 
 var education = {
   "schools": [
     {
       "name": "",
-      "city": "",
+      "city": "Portland, OR",
       "degree": "",
       "majors": "",
       "dates": "",
@@ -29,7 +29,7 @@ var education = {
     },
     {
       "name": "",
-      "city": "",
+      "city": "Portland, OR",
       "degree": "",
       "majors": "",
       "dates": "",
@@ -40,19 +40,19 @@ var education = {
     {
       "title": "",
       "school": "",
-      "dates": "",
+      "dates": ",",
       "url": ""
     },
     {
       "title": "",
       "school": "",
-      "dates": "",
+      "dates": ",",
       "url": ""
     },
     {
       "title": "",
       "school": "",
-      "dates": "",
+      "dates": ",",
       "url": ""
     }
   ]
@@ -61,22 +61,25 @@ var education = {
 var work = {
   "jobs": [
     {
-      "employer": "",
-      "title": "",
-      "dates": "",
-      "description": ""
+      "employer": "Employer",
+      "title": "Title",
+      "dates": "2000-2010",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nibh ligula, interdum vel sem vitae, pellentesque tristique erat. Curabitur vel ipsum sodales, maximus erat id, mattis mauris. Aliquam eget odio ullamcorper, luctus erat ut, consectetur augue. Aenean ut massa at augue sagittis pharetra in quis orci. Vestibulum consequat et leo eu pulvinar. Donec odio massa, ultrices vitae iaculis in, porttitor nec odio. Donec justo dolor, sagittis nec mauris quis, rutrum tempus sapien. Fusce lacinia mollis ex nec euismod. Cras tristique, tellus quis pretium vestibulum, metus metus dictum tortor, efficitur ornare eros sem vel tellus. Maecenas velit enim, aliquet nec dapibus semper, auctor non est. Donec congue sodales tellus, ut interdum nunc vestibulum viverra. Vestibulum mattis nulla in ligula pulvinar, eu tristique turpis aliquet. Vivamus sagittis turpis purus, rhoncus maximus nibh porttitor id.",
+      "location": "Portland, OR"
     },
     {
-      "employer": "",
-      "title": "",
-      "dates": "",
-      "description": ""
+      "employer": "Employer",
+      "title": "Title",
+      "dates": "2000-2010",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nibh ligula, interdum vel sem vitae, pellentesque tristique erat. Curabitur vel ipsum sodales, maximus erat id, mattis mauris. Aliquam eget odio ullamcorper, luctus erat ut, consectetur augue. Aenean ut massa at augue sagittis pharetra in quis orci. Vestibulum consequat et leo eu pulvinar. Donec odio massa, ultrices vitae iaculis in, porttitor nec odio. Donec justo dolor, sagittis nec mauris quis, rutrum tempus sapien. Fusce lacinia mollis ex nec euismod. Cras tristique, tellus quis pretium vestibulum, metus metus dictum tortor, efficitur ornare eros sem vel tellus. Maecenas velit enim, aliquet nec dapibus semper, auctor non est. Donec congue sodales tellus, ut interdum nunc vestibulum viverra. Vestibulum mattis nulla in ligula pulvinar, eu tristique turpis aliquet. Vivamus sagittis turpis purus, rhoncus maximus nibh porttitor id.",
+      "location": "Portland, OR"
     },
     {
-      "employer": "",
-      "title": "",
-      "dates": "",
-      "description": ""
+      "employer": "Employer",
+      "title": "Title",
+      "dates": "2000-2010",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nibh ligula, interdum vel sem vitae, pellentesque tristique erat. Curabitur vel ipsum sodales, maximus erat id, mattis mauris. Aliquam eget odio ullamcorper, luctus erat ut, consectetur augue. Aenean ut massa at augue sagittis pharetra in quis orci. Vestibulum consequat et leo eu pulvinar. Donec odio massa, ultrices vitae iaculis in, porttitor nec odio. Donec justo dolor, sagittis nec mauris quis, rutrum tempus sapien. Fusce lacinia mollis ex nec euismod. Cras tristique, tellus quis pretium vestibulum, metus metus dictum tortor, efficitur ornare eros sem vel tellus. Maecenas velit enim, aliquet nec dapibus semper, auctor non est. Donec congue sodales tellus, ut interdum nunc vestibulum viverra. Vestibulum mattis nulla in ligula pulvinar, eu tristique turpis aliquet. Vivamus sagittis turpis purus, rhoncus maximus nibh porttitor id.",
+      "location": "Portland, OR"
     }
   ]
 };
@@ -84,16 +87,76 @@ var work = {
 var projects = {
   "projects": [
     {
-      "title": "",
-      "dates": "",
-      "description": "",
-      "images": ["link", "link"]
+      "title": "ddd",
+      "dates": "ssss",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nibh ligula, interdum vel sem vitae, pellentesque tristique erat. Curabitur vel ipsum sodales, maximus erat id, mattis mauris. Aliquam eget odio ullamcorper, luctus erat ut, consectetur augue. Aenean ut massa at augue sagittis pharetra in quis orci. Vestibulum consequat et leo eu pulvinar. Donec odio massa, ultrices vitae iaculis in, porttitor nec odio. Donec justo dolor, sagittis nec mauris quis, rutrum tempus sapien. Fusce lacinia mollis ex nec euismod. Cras tristique, tellus quis pretium vestibulum, metus metus dictum tortor, efficitur ornare eros sem vel tellus. Maecenas velit enim, aliquet nec dapibus semper, auctor non est. Donec congue sodales tellus, ut interdum nunc vestibulum viverra. Vestibulum mattis nulla in ligula pulvinar, eu tristique turpis aliquet. Vivamus sagittis turpis purus, rhoncus maximus nibh porttitor id.",
+      "images": ["https://dl.dropboxusercontent.com/u/633848/Udacity-forum/truthy%20falsy%20js2.png", "https://dl.dropboxusercontent.com/u/633848/Udacity-forum/truthy%20falsy%20js2.png"]
     },
     {
-      "title": "",
-      "dates": "",
-      "description": "",
-      "images": ["link", "link"]
+      "title": "sss",
+      "dates": "sss",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nibh ligula, interdum vel sem vitae, pellentesque tristique erat. Curabitur vel ipsum sodales, maximus erat id, mattis mauris. Aliquam eget odio ullamcorper, luctus erat ut, consectetur augue. Aenean ut massa at augue sagittis pharetra in quis orci. Vestibulum consequat et leo eu pulvinar. Donec odio massa, ultrices vitae iaculis in, porttitor nec odio. Donec justo dolor, sagittis nec mauris quis, rutrum tempus sapien. Fusce lacinia mollis ex nec euismod. Cras tristique, tellus quis pretium vestibulum, metus metus dictum tortor, efficitur ornare eros sem vel tellus. Maecenas velit enim, aliquet nec dapibus semper, auctor non est. Donec congue sodales tellus, ut interdum nunc vestibulum viverra. Vestibulum mattis nulla in ligula pulvinar, eu tristique turpis aliquet. Vivamus sagittis turpis purus, rhoncus maximus nibh porttitor id.",
+      "images": ["https://dl.dropboxusercontent.com/u/633848/Udacity-forum/truthy%20falsy%20js2.png", "https://dl.dropboxusercontent.com/u/633848/Udacity-forum/truthy%20falsy%20js2.png"]
     }
   ]
 };
+
+// Add information to Bio ***************************************
+bio.display = function() {
+  // Add Name and Role 
+  var formattedName = HTMLheaderName.replace("%data%", bio.name);
+  var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+
+  $("#header").prepend(formattedRole);
+  $("#header").prepend(formattedName);
+
+  // Add contact details
+  var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+  var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+  var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+  var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+  var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+
+  $("#topContacts").prepend(formattedMobile);
+  $("#topContacts").prepend(formattedEmail);
+  $("#topContacts").prepend(formattedLocation);
+  $("#topContacts").prepend(formattedGithub);
+  $("#topContacts").prepend(formattedTwitter);
+
+  // Add picture and welcome message
+  var formattedBiopic = HTMLbioPic.replace("%data%", bio.biopic);
+  var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+  $("#header").append(formattedBiopic);
+  $("#header").append(formattedWelcomeMessage);
+
+  // Add skills list tags
+  $("#header").append(HTMLskillsStart);
+
+  // and populate skills
+  if(bio.skills.length > 0) {
+    for(skill in bio.skills) {
+      var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
+      $("#skills").prepend(formattedSkill);
+    }
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Add infomation to Work Experiance ****************************
+// Add information to Projects **********************************
+// Add information to Education *********************************
+// Turn on Google Map *******************************************
+bio.display();
