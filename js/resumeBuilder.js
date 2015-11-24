a9f81ac1729b3aba07a77ae53da074f68a461208
Sylwia M. Bialczak <argoncode18@gmail.com>
2015-11-23 22:11:54 -0800

    Add empty objects for Resume section. Clean index.html comments.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,99 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+    "name": "",
+    "role": "",
+    "contacts": {
+        "mobile": "",
+        "twitter": "",
+        "github": "",
+        "location": "",
+        "facebook": "",
+        "googlePlus": "",
+        "linkedin": "",
+        "email": "",
+        "kaybase": ""
+    },
+    "biopic": "",
+    "welcomeMessage": "",
+    "skills": []
+};
+
+var education = {
+  "schools": [
+    {
+      "name": "",
+      "city": "",
+      "degree": "",
+      "majors": "",
+      "dates": "",
+      "url": ""
+    },
+    {
+      "name": "",
+      "city": "",
+      "degree": "",
+      "majors": "",
+      "dates": "",
+      "url": ""
+    }
+  ],
+  "onlineCourses": [
+    {
+      "title": "",
+      "school": "",
+      "dates": "",
+      "url": ""
+    },
+    {
+      "title": "",
+      "school": "",
+      "dates": "",
+      "url": ""
+    },
+    {
+      "title": "",
+      "school": "",
+      "dates": "",
+      "url": ""
+    }
+  ]
+};
+
+var work = {
+  "jobs": [
+    {
+      "employer": "",
+      "title": "",
+      "dates": "",
+      "description": ""
+    },
+    {
+      "employer": "",
+      "title": "",
+      "dates": "",
+      "description": ""
+    },
+    {
+      "employer": "",
+      "title": "",
+      "dates": "",
+      "description": ""
+    }
+  ]
+};
+
+var projects = {
+  "projects": [
+    {
+      "title": "",
+      "dates": "",
+      "description": "",
+      "images": ["link", "link"]
+    },
+    {
+      "title": "",
+      "dates": "",
+      "description": "",
+      "images": ["link", "link"]
+    }
+  ]
+};
